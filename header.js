129f18e772b02aea19b3c1bdd432b173c046fbdc
Kyle Ogilvie <kogilvie@2u.com>
2020-09-28 11:44:11 -0400

    working single page calculator

---
+++ header.js
@@ -0,0 +1,46 @@
+<style>
+  body {
+    font-family: Helvetica, Arial, sans-serif;
+    text-align: center;
+  }
+  .bigtext {
+    font-size: 2em;
+  }
+  .mediumtext {
+    font-size: 1.5em;
+  }
+  .smalltext {
+    font-size: .5em;
+  }
+  .btnbig {
+    background-color: #1652DF;
+    color: white;
+    border: 0px;
+    font-size: 1.5em;
+    padding: 15px;
+    margin: 15px;
+  }
+  .btnsmall {
+    background-color: #1652DF;
+    color: white;
+    font-size: 1.25em;
+    padding: 15px;
+    border: 0px;
+    margin: 15px;
+  }
+  #site-title{
+  	display: none;
+  }
+ .page-section.section-height--small:not(.content-collection):not(.gallery-section) {
+    min-height: 0vh;
+}
+  header{
+    display: none;
+  }
+  .button{
+    display: inline-block;
+}
+</style>
+<div id="who" style="display: none;">
+We'd also like him gone. Find us at https://www.kyleogilvie.com/ & https://www.maryannbadavi.com/ .
+</div>
